cd3da33a1cf7368e274f71d163b918c60a0c9db6
ekkasit44 <phoenixsrisuk@gmail.com>
2025-12-03 09:21:31 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿namespace BMI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("กรอกน้ำหนัก");
+            string weightInput = Console.ReadLine();
+            double weight = double.Parse(weightInput);
+            Console.WriteLine("กรอกส่วนสูง (เมตร)");
+            string heightInput = Console.ReadLine();
+            double height = double.Parse(heightInput);
+            double bmi = weight / (height * height);
+            Console.WriteLine("ค่าดัชนีมวลกาย (BMI) ของคุณคือ: " + bmi);
+            if (bmi < 18.5)
+            {
+                Console.WriteLine("น้ำหนักน้อยเกินไป");
+            }
+            else if (bmi >= 18.5 && bmi < 24.9)
+            {
+                Console.WriteLine("น้ำหนักปกติ");
+            }
+            else if (bmi >= 25 && bmi < 29.9)
+            {
+                Console.WriteLine("น้ำหนักเกิน");
+            }
+            else
+            {
+                Console.WriteLine("โรคอ้วน");
+            }
+
+        }
+    }
+}
+
